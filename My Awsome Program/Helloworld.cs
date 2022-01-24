2490f66c38376a96fc8d94636a6115de3bcf81c0
Omkarpawar543 <79417706+Omkarpawar543@users.noreply.github.com>
2022-01-24 20:58:17 +0530

    first program in C#

---
+++ My Awsome Program/Helloworld.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Omkar";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+            Console.WriteLine("Hello World!!");
+            Console.ReadKey();
+        }
+    }
+}
