48f260352bd36113d486d4e3d36b6d29c86cb0b4
Omkarpawar543 <79417706+Omkarpawar543@users.noreply.github.com>
2022-01-26 14:13:37 +0530

    Story Mode - Normal Convo
    Story Mode - Normal Conversation between 2 participants of a GameJam!

--- My Awsome Program/Helloworld.cs
+++ My Awsome Program/Helloworld.cs
@@ -7,10 +7,26 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.Title = "Omkar";
-            Console.ForegroundColor = ConsoleColor.Green;
+            
             Console.WindowHeight = 40;
-            Console.WriteLine("Hello World!!");
-            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("*In Outscale's Game Jam*");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Hello my name is Jack! and you are...?");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Nice to meet you buddy");
+            Console.WriteLine("Which game are you making for this Game Jam!");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("I will go with some 2d combat types mostly!");
+            Console.WriteLine("BTW.. All the best will meet you at the end of the Jam!!");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Bye!!");          
         }
     }
 }
