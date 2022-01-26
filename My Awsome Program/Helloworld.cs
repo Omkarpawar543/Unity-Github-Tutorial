519a180e412c7d61dbfdc30c66a0c1eb5d72d45b
Omkarpawar543 <79417706+Omkarpawar543@users.noreply.github.com>
2022-01-26 14:39:23 +0530

    Average of 3 numbers

--- My Awsome Program/Helloworld.cs
+++ My Awsome Program/Helloworld.cs
@@ -1,32 +1,29 @@
 using System;
 
-namespace HelloWorld
+namespace Avgof3numbers
 {
     class Program1
     {
         static void Main(string[] args)
         {
-            Console.Title = "Omkar";
-            
-            Console.WindowHeight = 40;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("*In Outscale's Game Jam*");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Hello my name is Jack! and you are...?");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Nice to meet you buddy");
-            Console.WriteLine("Which game are you making for this Game Jam!");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("I will go with some 2d combat types mostly!");
-            Console.WriteLine("BTW.. All the best will meet you at the end of the Jam!!");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Bye!!");          
+            double numx;
+            double numy;
+            double numz;
+
+            Console.Write("Input a number: ");
+            numx = Convert.ToDouble(Console.ReadLine() );
+
+            Console.Write("Input a second number: ");
+            numy = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input a third number: ");
+            numz = Convert.ToDouble(Console.ReadLine());
+
+            double Result = (numx + numy + numz)/3;
+            Console.WriteLine("The Result is " + Result);
+
+            Console.ReadKey();
+
         }
     }
 }
