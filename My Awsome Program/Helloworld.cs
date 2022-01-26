ab21928016f4d0094c8f1c2d0b7b4ce75a594992
Omkarpawar543 <79417706+Omkarpawar543@users.noreply.github.com>
2022-01-26 16:48:32 +0530

    Program for ticket collection
    Program for ticket collection

--- My Awsome Program/Helloworld.cs
+++ My Awsome Program/Helloworld.cs
@@ -6,24 +6,23 @@ namespace Avgof3numbers
     {
         static void Main(string[] args)
         {
-            double numx;
-            double numy;
-            double numz;
-
-            Console.Write("Input a number: ");
-            numx = Convert.ToDouble(Console.ReadLine() );
-
-            Console.Write("Input a second number: ");
-            numy = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Input a third number: ");
-            numz = Convert.ToDouble(Console.ReadLine());
-
-            double Result = (numx + numy + numz)/3;
-            Console.WriteLine("The Result is " + Result);
+            Console.WriteLine("Welcome! Tickets are 5$. Please insert cash.");
+            int cash = Convert.ToInt32(Console.ReadLine());
+            if (cash < 5)
+            {
+                Console.WriteLine("That's not enough money.");
+            }
+            else if (cash == 5)
+            {
+                Console.WriteLine("Here's your Ticket.");
+            }
+            else 
+            {
+                int change = cash - 5;
+                Console.WriteLine("Here is your ticket and " + change + " dollars in change.");
+            }
 
             Console.ReadKey();
-
         }
     }
 }
