24fb084c12c920ee9bffb759624d90e43be20f58
Omkarpawar543 <79417706+Omkarpawar543@users.noreply.github.com>
2022-02-09 13:33:20 +0530

    Program for Math problems
    Program for Math problems

--- My Awsome Program/Helloworld.cs
+++ My Awsome Program/Helloworld.cs
@@ -6,21 +6,45 @@ namespace Avgof3numbers
     {
         static void Main(string[] args)
         {
-            int age;
-            int height;
+            int a;
+            int b;
+            int c;
 
-            Console.WriteLine("Please input the age: ");
-            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("18+4-1=");
+            a = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Please input height in (cms): ");
-            height = Convert.ToInt32(Console.ReadLine());
-
-            if(age >= 18 && height >= 160) {
-                Console.WriteLine("Now you can enter!");
-            }else {
-                Console.WriteLine("You don't meet requirements.");
+            if(a == 21) 
+            {
+                Console.WriteLine("Correct!");
+            }
+            else 
+            {
+                System.Environment.Exit(0); 
             }
             
+            Console.WriteLine("15*5=");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if(b == 75) 
+            {
+                Console.WriteLine("Correct!");
+            }
+            else 
+            {
+                Console.WriteLine("Wrong!");
+            }
+
+            Console.WriteLine("18/2 + 12=");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if(c == 21) 
+            {
+                Console.WriteLine("Correct!");
+            }
+            else 
+            {
+                Console.WriteLine("Wrong!");
+            }
             Console.ReadKey();
         }
     }
